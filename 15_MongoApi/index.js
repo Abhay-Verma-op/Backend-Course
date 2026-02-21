0098481259c33291905c55d7b767dfd652490c14
Abhay-Verma-op <abhayzwart7505675870@gmail.com>
2026-02-21 15:56:48 +0530

    Momgo & MongooseStart

--- 15_MongoApi/index.js
+++ 15_MongoApi/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const dbName="school"
 
@@ -47,6 +47,54 @@ client.connect().then((connection)=>{
         // resp.send({"message":"success"})
         // resp.send({"message":req.body})
         resp.send({message:"Data stored",success:true , result:result})
+    })
+    app.delete('/delete/:id',async(req,resp)=>{
+        console.log(req.params.id)
+        const collection=db.collection('students')
+        const result=await collection.deleteOne({_id: new ObjectId(req.params.id)})
+        if(result){
+            resp.send({
+                message:"Student Data Deleted",
+                success:true
+            })
+         } else{
+            resp.send({
+                message:"Student Data not Deleted try afater sometime",
+                success:false
+            })
+        }
+        // resp.send("working")
+    })
+    app.get('/ui/delete/:id',async(req,resp)=>{
+        console.log(req.params.id)
+        const collection=db.collection('students')
+        const result=await collection.deleteOne({_id: new ObjectId(req.params.id)})
+        if(result){
+            resp.send("<h1>Student Record Deleted</h1>")
+
+         } else{
+         resp.send("<h1>Student Record not Deleted</h1>")
+        }
+        // resp.send("working")
+    })
+
+    app.get('/ui/students/:id',async(req,resp)=>{
+        const id=req.params.id
+        console.log(id);
+        const collection=db.collection('students')
+        const result=await collection.findOne({_id: new ObjectId(req.params.id)})
+        resp.render( 'update-student',{result})
+    })
+    app.get('/students/:id',async(req,resp)=>{
+        const id=req.params.id
+        console.log(id);
+        const collection=db.collection('students')
+        const result=await collection.findOne({_id: new ObjectId(req.params.id)})
+        resp.send({
+            message:"data fetched",
+            success:true,
+            result:result
+        })
     })
 })
 
---
+++ 16_Moongoose/index.js
@@ -0,0 +1,33 @@
+// import mongoose from "mongoose"
+
+//  async function dbConnection(){
+//     await mongoose.connect("mongodb://localhost:27017/school")
+//     const schema=mongoose.Schema({
+//         name:String,
+//         email:String,
+//         age:Number,
+//     })
+//     const studentsModel=mongoose.model('students',schema)
+//     const result=await studentsModel.find()
+//     console.log(result);
+    
+//  }
+//  dbConnection()
+
+
+import mongoose from "mongoose"
+import express from 'express'
+import studentModel from "./model/studentModel.js";
+const app=express()
+
+ await mongoose.connect("mongodb://localhost:27017/school").then(()=>{
+    console.log("____connected____");
+    
+ })
+
+app.get('/',async(req,resp)=>{
+    const studentData=await studentModel.find()
+    resp.send({studentData})
+})
+
+app.listen(3200)
