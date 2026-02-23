3b951ea732faa16828ec5cd471b1fd01392014eb
Abhay-Verma-op <abhayzwart7505675870@gmail.com>
2026-02-23 22:33:54 +0530

    code-api

--- 16_Moongoose/index.js
+++ 16_Moongoose/index.js
@@ -19,7 +19,7 @@ import mongoose from "mongoose"
 import express from 'express'
 import studentModel from "./model/studentModel.js";
 const app=express()
-
+app.use(express.json())
  await mongoose.connect("mongodb://localhost:27017/school").then(()=>{
     console.log("____connected____");
     
@@ -28,6 +28,51 @@ const app=express()
 app.get('/',async(req,resp)=>{
     const studentData=await studentModel.find()
     resp.send({studentData})
+})
+app.post('/save',async(req,resp)=>{
+    console.log(req.body);
+    const {name,age,email}=req.body;
+    if(!req.body|| !name || !age || !email){
+        resp.send({
+        message:"Data Not Stored",
+        success:false,
+        storedInfo:null
+    })
+    return false
+    }
+    const studentData=await studentModel.create(req.body)
+    resp.send({
+        message:"Data Stored",
+        success:true,
+        storedInfo:studentData
+    })
+    
+    })
+    app.put('/update/:id',async(req,resp)=>{
+        const id=req.params.id
+        console.log(req.body,id);
+        const studentData=await studentModel.findByIdAndUpdate(id,{
+...req.body
+        })
+        resp.send({
+            message:"data Updated",
+            success:true,
+            info:studentData
+        })
+})
+
+
+    app.delete('/delete/:id',async(req,resp)=>{
+        const id=req.params.id
+        // console.log(req.body,id);
+        const studentData=await studentModel.findByIdAndDelete(id,{
+// ...req.body
+        })
+        resp.send({
+            message:"data deleted",
+            success:true,
+            info:studentData
+        })
 })
 
 app.listen(3200)
